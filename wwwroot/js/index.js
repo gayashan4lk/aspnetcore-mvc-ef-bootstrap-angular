62ba7bb389eaacbf99d31dab40423e65ab5e4642
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-06 00:01:14 +0530

    login form show hide with jquery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -7,18 +7,27 @@ buyButton.addEventListener("click", () => {
     console.log("proccess buying..")
 });*/
 
+(function () {
 
-let messageForm = $("#messageForm");
-messageForm.hide();
+    let messageForm = $("#messageForm");
+    messageForm.hide();
 
-let button = $("#buyButton");
-button.on("click", () => {
-    console.log("Buying process...");
-});
+    let button = $("#buyButton");
+    button.on("click", () => {
+        console.log("Buying process...");
+    });
 
-let productInfo = $(".product-props li");
+    let productInfo = $(".product-props li");
 
-// arrow function did not work here.
-productInfo.on("click", function () {
-    console.log("You clicked on " + $(this).text());
-});
+    // arrow function did not work here.
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+})(); // execute immediately
+
+let loginToggle = $("#loginToggle");
+let popupForm = $(".popup-form");
+
+loginToggle.on("click", function () {
+    popupForm.toggle(500);
+})
