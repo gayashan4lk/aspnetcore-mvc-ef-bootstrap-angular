54bb120c5395ed15212631618702adae40ad9252
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-05 22:23:14 +0530

    js events

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,3 +1,8 @@
 ﻿let messageForm = document.getElementById("messageForm");
 console.log(messageForm);
 //messageForm.hidden = true;
+
+let buyButton = document.getElementById("buyButton");
+buyButton.addEventListener("click", () => {
+    console.log("proccess buying..")
+});
