379d4f0f04e94220f7f3eb6324dc4f1190094ecf
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-05 23:24:48 +0530

    introducing jquery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,8 +1,24 @@
-﻿let messageForm = document.getElementById("messageForm");
-console.log(messageForm);
+﻿//let messageForm = document.getElementById("messageForm");
+//console.log(messageForm);
 //messageForm.hidden = true;
 
-let buyButton = document.getElementById("buyButton");
+/*let buyButton = document.getElementById("buyButton");
 buyButton.addEventListener("click", () => {
     console.log("proccess buying..")
+});*/
+
+
+let messageForm = $("#messageForm");
+messageForm.hide();
+
+let button = $("#buyButton");
+button.on("click", () => {
+    console.log("Buying process...");
+});
+
+let productInfo = $(".product-props li");
+
+// arrow function did not work here.
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
 });
