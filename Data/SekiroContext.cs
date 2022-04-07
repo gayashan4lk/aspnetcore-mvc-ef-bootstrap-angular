0a476d74c4b4da378f747b44fcb7f9bd63440319
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-07 17:01:14 +0530

    add DbContext

---
+++ Data/SekiroContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SekiroApp.Data.Entities;
+
+namespace SekiroApp.Data
+{
+    public class SekiroContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
